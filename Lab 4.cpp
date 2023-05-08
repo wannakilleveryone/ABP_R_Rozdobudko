7d2696f1020f469cd1ca7888c96f8c5a47df36f4
Kiril Rozdobudko <113985251+wannakilleveryone@users.noreply.github.com>
2023-05-08 10:54:11 +0300

    Add files via upload
    ASDLab4

---
+++ Lab 4.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Node {
+    int mItem;
+    Node *mNext;
+    Node(int item, Node *next)
+    {
+        mItem = item;
+        mNext = next;
+    };
+};
+
+int main()
+{
+    cout << "Vvedit kilkist lyudey ta interval vidalennya: ";
+    int N, M;
+    cin >> N >> M;
+    Node* first = new Node(1, 0);
+    first->mNext = first;
+    Node* temp = first;
+    cout << "Vidaleni elementi: \n";
+    for (int i = 2; i <= N; i++) {
+        temp->mNext = new Node(i, first);
+        temp = temp->mNext;
+    }
+    while (temp != temp->mNext) {
+        for (int i = 1; i < M; i++) {
+            temp = temp->mNext;
+        }
+        Node* deleteNode = temp->mNext;
+        temp->mNext = temp->mNext->mNext;
+
+        cout << deleteNode->mItem << "\n";
+        delete deleteNode;
+    }
+    cout << "Zalishivsya element: " << temp->mItem;
+}
+
